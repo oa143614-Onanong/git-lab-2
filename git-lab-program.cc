0a65791a978c7fdfd1f5b79056717a55deb20d0e
oa143614-Onanong <oa143614@ohio.edu>
2025-05-13 21:24:44 -0400

    first c++ file

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Onanong   Autama
+ *        Date: 5/13/2025
+ * Description: Add Description
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+ 
+ 
+ int main(int argc, char const *argv[]) {
+ 
+     /*add code*/
+     cout << "Hello Git!!" << endl;
+     return 0;
+ }// main
